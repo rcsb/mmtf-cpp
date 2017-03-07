29dc6f4b46e7f7e637afafaad97c22778dfc7aff
Gerardo Tauriello <gerardo.tauriello@unibas.ch>
2017-03-07 15:37:32 +0100

    Switched to simpler MIT license.

--- include/mmtf/binary_decoder.hpp
+++ include/mmtf/binary_decoder.hpp
@@ -1,18 +1,8 @@
 // *************************************************************************
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// Licensed under the MIT License (see accompanying LICENSE file).
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// The authors of this code are: Gabriel Studer, Gerardo Tauriello
+// The authors of this code are: Gerardo Tauriello
 // 
 // Based on mmtf_c developed by Julien Ferte (http://www.julienferte.com/),
 // Anthony Bradley, Thomas Holder with contributions from Yana Valasatava,
