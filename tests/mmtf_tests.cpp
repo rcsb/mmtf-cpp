3fc6169aa08f0a380fe86f933a71424e984be610
Danny Farrell <danpf@uw.edu>
2018-03-07 07:08:03 -0800

    Almost done top-level CMakeLists.txt

--- tests/mmtf_tests.cpp
+++ tests/mmtf_tests.cpp
@@ -2,3 +2,9 @@
 #include "catch.hpp"
 
 #include <mmtf.hpp>
+
+// Tests for structure_data.hpp
+TEST_CASE("Test for equality of StructureData") {
+	mmtf::StructureData sd1, sd2;
+	REQUIRE( sd1 == sd2 );
+}
