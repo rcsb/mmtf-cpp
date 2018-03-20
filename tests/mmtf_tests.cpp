075f979b2fe25e945264d71ccfb59447e9413e83
Danny Farrell <danpf@uw.edu>
2018-03-20 00:11:02 -0700

    Delta recursive float test added

--- tests/mmtf_tests.cpp
+++ tests/mmtf_tests.cpp
@@ -3,6 +3,25 @@
 
 #include <mmtf.hpp>
 
+
+// we never use anything less than 0.xxx, this is more than enough
+// precision for the PDB. Perhaps more investigation will be necessary
+// in the future.
+template <typename T>
+bool almost_equal(const T& a, const T& b) {
+	return std::abs(a - b) < 0.00001;
+}
+
+
+template <typename T>
+bool almost_equal_vector(const T& a, const T& b) {
+	for (std::size_t i=0; i<=a.size(); ++i) {
+		if (!almost_equal(a[i], b[i])) return false;
+	}
+	return true;
+}
+
+
 // Tests for structure_data.hpp
 TEST_CASE("Test for equality of StructureData") {
 	mmtf::StructureData sd1, sd2;
@@ -10,19 +29,46 @@ TEST_CASE("Test for equality of StructureData") {
 }
 
 TEST_CASE("Test DeltaRecursiveFloat enc/dec") {
-	//const char* encoded_data = '\x7f\xffD\xab\x01\x8f\xff\xca';
-	char encoded_data[] = "\x7f\xffD\xab\x01\x8f\xff\xca";
-	//const char* encoded_data = u"\x007f\x0ffD\x00ab\x0001\x008f\x00ff\x00ca";
-	//const char* encoded_data = "\u7f\uffD\uab\u01\u8f\uff\uca";
-	//const char* encoded_data = "0x7f0xffD0xab0x010x8f0xff0xca";
+	std::vector<char> encoded_data;
+	// h1
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x0a);
+	// h2
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x03);
+	// h3
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x00);
+	encoded_data.push_back(0x03);
+	encoded_data.push_back(0xe8);
+	// data
+	encoded_data.push_back(0x7f);
+	encoded_data.push_back(0xff);
+	encoded_data.push_back(0x44);
+	encoded_data.push_back(0xab);
+	encoded_data.push_back(0x01);
+	encoded_data.push_back(0x8f);
+	encoded_data.push_back(0xff);
+	encoded_data.push_back(0xca);
+
+	msgpack::zone m_zone;
+	msgpack::object msgp_obj(encoded_data, m_zone);
+
+	mmtf::BinaryDecoder bd(msgp_obj, "a_test");
+	std::vector<float> decoded_input;
+	bd.decode(decoded_input);
+
 	std::vector<float> decoded_data;
-	decoded_data.push_back(50.346);
-	decoded_data.push_back(50.745);
-	decoded_data.push_back(50.691);
-	std::cout << "encoded: " << std::string(encoded_data) << std::endl;
-//std::cout << "decoded: " << mmtf::encodeDeltaRecursiveFloat(decoded_data, 1000) << std::endl;
-	std::vector<char> encode_output = mmtf::encodeDeltaRecursiveFloat(decoded_data, 1000);
-	std::string encode_output_str(encode_output.begin(),encode_output.end());
-	std::cout << "encoded: " << encode_output_str << std::endl;
-	REQUIRE(std::string(encoded_data) == encode_output_str);
+	decoded_data.push_back(50.346f);
+	decoded_data.push_back(50.745f);
+	decoded_data.push_back(50.691f);
+
+	std::vector<char> encoded_output = mmtf::encodeDeltaRecursiveFloat(decoded_data, 1000);
+	REQUIRE(encoded_data == encoded_output);
+	REQUIRE(decoded_data.size() ==  decoded_input.size());
+	REQUIRE(almost_equal_vector(decoded_data, decoded_input));
 }
