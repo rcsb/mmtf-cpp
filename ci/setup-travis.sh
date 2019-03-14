8b482feeb1dd91b8ea98661bbcadbf2f5fb63cf3
Gerardo Tauriello <gerardo.tauriello@unibas.ch>
2019-03-14 20:40:10 +0100

    Remove debug output.

--- ci/setup-travis.sh
+++ ci/setup-travis.sh
@@ -33,15 +33,6 @@ fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     if [[ "$CC" == "gcc" ]]; then
-        echo "--------------------------------"
-        echo $CXX
-        which gcc
-        which g++
-        ls -l $(which g++)
-        echo "--------------------------------"
-        which g++-6
-        ls -l $(which g++-6)
-        echo "--------------------------------"
         export CC=gcc-6
         export CXX=g++-6
     fi
