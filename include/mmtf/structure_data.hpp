b02a7ce79180171032cac39afc00ac1cf6a4b0e1
Gerardo Tauriello <gerardo.tauriello@unibas.ch>
2019-03-14 19:39:46 +0100

    Remove C++11 code again.

--- include/mmtf/structure_data.hpp
+++ include/mmtf/structure_data.hpp
@@ -440,12 +440,10 @@ bool is_polymer(const unsigned int chain_index,
               || entity_list[i].type == "POLYMER");
     }
   }
-  // std::stringstream err;
-  // err << "'is_polymer' unable to find chain_index: " << chain_index
-  //     << " in entity list";
-  // throw DecodeError(err.str());
-  throw DecodeError("'is_polymer' unable to find chain_index: "
-                    + std::to_string(chain_index) + " in entity list");
+  std::stringstream err;
+  err << "'is_polymer' unable to find chain_index: " << chain_index
+      << " in entity list";
+  throw DecodeError(err.str());
 }
 
 bool is_hetatm(const char* type) {
