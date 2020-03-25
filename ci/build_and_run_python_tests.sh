4c3414ed3a2052d4fa69054c08dae3cae2e61def
Danny Farrell <danpf@uw.edu>
2020-03-25 01:17:42 -0700

    set catch2

--- ci/build_and_run_python_tests.sh
+++
@@ -1,9 +0,0 @@
-
-python3 --version
-pip3 --version
-pip3 install pytest pytest-cov
-cd $TRAVIS_BUILD_DIR
-pip3 install .
-pytest python_src/tests/tests.py -s -vv
-
-
--- ci/build_and_run_tests.sh
+++
@@ -1,7 +0,0 @@
-cd $TRAVIS_BUILD_DIR
-mkdir build && cd build
-$CMAKE_CONFIGURE cmake $CMAKE_ARGS $CMAKE_EXTRA ..
-make -j2
-ctest -j2 --output-on-failure
-bash $TRAVIS_BUILD_DIR/ci/travis-test-example.sh
-cd $TRAVIS_BUILD_DIR
