37d232b6db3d79b3a9f2fd4e31fab59d233b3b83
Gerardo Tauriello <gerardo.tauriello@unibas.ch>
2020-10-23 14:53:14 +0200

    Fix segmentation fault with -march=native (#40)

---
+++ ci/build_and_run_tests.sh
@@ -0,0 +1,8 @@
+set -e
+cd $TRAVIS_BUILD_DIR
+mkdir build && cd build
+$CMAKE_CONFIGURE cmake $CMAKE_ARGS $CMAKE_EXTRA ..
+make -j2
+ctest -j2 --output-on-failure
+bash $TRAVIS_BUILD_DIR/ci/travis-test-example.sh
+cd $TRAVIS_BUILD_DIR
